3763305bc18a6b0e3e46b99442aa8a0e7b5aa8b9
justinrbeattie <justin.r.beattie@gmail.com>
2023-07-01 16:22:41 +1000

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import { builderDevTools } from "@builder.io/dev-tools/vite";
+import { partytownVite } from "@builder.io/partytown/utils";
+import { join } from "path";
 
 export default defineConfig(() => {
   return {
-    plugins: [builderDevTools(), qwikCity(), qwikVite()],
+    plugins: [
+      builderDevTools(),
+      qwikCity(),
+      qwikVite(),
+      partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
+    ],
   };
 });
